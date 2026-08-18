159645e731d4f94ef94b8d754cac35ab2dc07bbb
SanjayKomma <sanjukomma1@gmail.com>
2026-08-18 16:18:14 +0530

    added all the backend part

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dns = require('dns');
 const app = require("./app");
+const cors = require('cors');
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 const { MONGODB_URI, PORT, HOST } = require('./utils/config');
 const mongoose = require('mongoose');
@@ -18,3 +19,20 @@ mongoose
     .catch((err) => {
         console.log("Error connecting to MongoDB", err.message);
     });
+const allowedOrigins = [
+  'http://localhost:5173', // Local Vite dev
+  process.env.CLIENT_URL,  // Deployed frontend URL
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // Crucial for HTTP-only cookies across domains
+  })
+);
