1898b198d1ca6e321ff7699a333d9a08689c6056
SanjayKomma <sanjukomma1@gmail.com>
2026-08-19 14:14:57 +0530

    ..

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/authRouter');
 const productRouter = require('./routes/productRouter');
