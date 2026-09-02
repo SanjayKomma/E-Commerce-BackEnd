0d5cc071aead3862654ede2ce36ca4e017dda625
SanjayKomma <sanjukomma1@gmail.com>
2026-09-03 00:28:14 +0530

    updated user model according to the requirement

--- models/user.js
+++ models/user.js
@@ -1,51 +1,75 @@
 const mongoose = require('mongoose');
+
 const addressSchema = new mongoose.Schema({
-    street:{
-        type:String,
+    street: {
+        type: String,
     },
-    city:{
-        type:String,
+    city: {
+        type: String,
     },
-    ZipCode:{
-        type:String,
+    ZipCode: {
+        type: String,
     },
-    country:{
-        type:String
+    country: {
+        type: String,
     },
-    isDefault:{
-        type:Boolean,
-        default:false
-    }
-})
-const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+    isDefault: {
+        type: Boolean,
+        default: false,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+});
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: ['admin', 'buyer', 'seller'],
+            default: 'buyer',
+        },
+        phone: {
+            type: String,
+        },
+        address: [addressSchema],
+
+        storeName: {
+            type: String,
+            default: '',
+        },
+        tagline: {
+            type: String,
+            default: '',
+        },
+        bio: {
+            type: String,
+            default: '',
+        },
+        shippingPolicy: {
+            type: String,
+            default: '',
+        },
     },
-    password:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        enum:['admin','buyer','seller'],
-        default:'buyer'
-    },
-    phone:{
-        type:String,
-    },
-    address:[addressSchema],
-}, {timestamps: true});
+    { timestamps: true }
+);
+
 userSchema.set('toJSON', {
     transform: (doc, ret) => {
         delete ret.password;
         delete ret.__v;
         return ret;
-    }
+    },
 });
+
 module.exports = mongoose.model('User', userSchema);
