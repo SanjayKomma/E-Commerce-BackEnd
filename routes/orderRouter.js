0634d76233342e3c09c3eebb302c2f62ea1647f8
SanjayKomma <sanjukomma1@gmail.com>
2026-09-02 23:07:00 +0530

    updated a bug

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const {isAuthenticated} = require('../middlewares/auth');
+const {isAuthenticated, allowRoles} = require('../middlewares/auth');
 const { createOrder, getMyOrders, getOrderById, cancelOrder, getSellerOrders, updateItemShipmentStatus } = require('../controllers/orderController');
 const orderRouter = express.Router();
 orderRouter.use(isAuthenticated);
 orderRouter.post('/create', createOrder);
 orderRouter.get('/', getMyOrders);
-orderRouter.get('/seller/orders', isAuthenticated,allowRoles('seller', 'admin'), getSellerOrders);
+orderRouter.get('/seller/orders', isAuthenticated, allowRoles('seller', 'admin'), getSellerOrders);
 orderRouter.put('/:id/items/:id/status', isAuthenticated, allowRoles('admin', 'seller'), updateItemShipmentStatus);
 orderRouter.get('/:id', getOrderById);
 orderRouter.patch('/:id', cancelOrder);
